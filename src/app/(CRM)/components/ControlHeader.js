21b66a810146e513c6d8c24fd602ae7f10205876
Jakub Wojtysiak <it.jakub.wojtysiak@gmail.com>
2023-07-14 17:10:29 +0200

    Started Working with Dashboard main content

---
+++ src/app/(CRM)/components/ControlHeader.js
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import GreenPlusIcon from "@/images/icons/greenPlusIcon.png";
+import GreenExportIcon from "@/images/icons/greenExportIcon.png";
+import Link from "next/link";
+
+export default function ControlHeader() {
+  return (
+    <header>
+      <h1>Zlecenia</h1>
+      <div className="info">
+        <div className="stats">
+          <div className="statTile all">
+            <p className="statName">
+              <span>233</span> Zleceń
+            </p>
+          </div>
+          <div className="statTile new">
+            <p className="statName">
+              <span>12</span> Nowych Zleceń
+            </p>
+          </div>
+          <div className="statTile current">
+            <p className="statName">
+              <span>58</span> Bieżących Zleceń
+            </p>
+          </div>
+          <div className="statTile warehouse">
+            <p className="statName">
+              <span>62</span> Paczek na Magazynie
+            </p>
+          </div>
+          <div className="statTile realized">
+            <p className="statName">
+              <span>2243</span> Zrealizowanych Zleceń
+            </p>
+          </div>
+        </div>
+        <div className="actions">
+          <div href="/dashboard/newOrder" className="eksportOrders">
+            <Image src={GreenExportIcon} alt="Ikona eksportu zamwień" />
+            <p>Eksportuj</p>
+          </div>
+          <Link href="/dashboard/newOrder" className="newOrder">
+            <Image src={GreenPlusIcon} alt="Ikona dodawania nowego zlecenia" />
+            <p>Nowe Zlecenie</p>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
--- src/app/(CRM)/dashboard/page.js
+++ src/app/(CRM)/dashboard/page.js
@@ -1,16 +1,39 @@
-import Image from "next/image";
-import Link from "next/link";
-import SideBar from "../components/SideBar";
+import ControlHeader from "../components/ControlHeader";
+import ControlSideBar from "../components/ControlSideBar";
 
 export default function Dashboard() {
   return (
-    <>
-      <div className="leftSidebar">
-        <SideBar />
+    <div className="CrmPage">
+      <ControlSideBar />
+      <div className="mainContent">
+        <ControlHeader />
+        <main>
+          <table>
+            <thead>
+              <tr>
+                <th className="col1">Eksport</th>
+                <th className="col2">Numer Paczki</th>
+                <th className="col3">Status</th>
+                <th className="col4">Aktualizacja</th>
+                <th className="col5">Nazwa Klienta</th>
+                <th className="col6">Adres</th>
+                <th className="col7">Opcje</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="col1"><input type="checkbox" /></td>
+                <td className="col2">123456789</td>
+                <td className="col3">123456789</td>
+                <td className="col4">123456789</td>
+                <td className="col5">123456789</td>
+                <td className="col6">123456789</td>
+                <td className="col7">123456789</td>
+              </tr>
+            </tbody>
+          </table>
+        </main>
       </div>
-      <div>
-        <main className="crm"></main>
-      </div>
-    </>
+    </div>
   );
 }
