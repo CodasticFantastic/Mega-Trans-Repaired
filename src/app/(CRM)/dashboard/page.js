bd342c3938ffa3c4b3ae93fcf4d5d6341c04d3f3
Jakub Wojtysiak <it.jakub.wojtysiak@gmail.com>
2023-07-19 11:43:22 +0200

    Work with dashobard data

--- src/app/(CRM)/dashboard/page.js
+++ src/app/(CRM)/dashboard/page.js
@@ -6,18 +6,13 @@ import FilterSideBar from "../components/sidebars/FilterSideBar";
 
 import { useSession } from "next-auth/react";
 import Link from "next/link";
-import EditIcon from "@/images/icons/editIcon.png";
-import CircleArrowDownIcon from "@/images/icons/circleArrowDownIcon.png";
-import PhoneIcon from "@/images/icons/phoneIcon.png";
-import EmailIcon from "@/images/icons/emailIcon.png";
-import CompanyIcon from "@/images/icons/companyIcon.png";
 
 import Image from "next/image";
+import TableDataRow from "@/app/(Website)/components/TableDataRow";
 
 export default function Dashboard() {
   const { data: session } = useSession();
   const [userOrders, setUserOrders] = useState([]);
-  // let date = new Date();
 
   useEffect(() => {
     if (session) {
@@ -29,36 +24,15 @@ export default function Dashboard() {
     let request = await fetch(`http://localhost:3000/api/order/showAllOrders`, { headers: { Authorization: session?.accessToken } });
 
     let response = await request.json();
+    console.log(response);
 
     setUserOrders(
       response.allUserOrder.map((order) => {
-        return (
-          <tr key={order.orderId}>
-            <td className="col1">
-              <input type="checkbox" value={order.orderId} />
-            </td>
-            <td className="col2">{order.orderId}</td>
-            <td className="col3">{order.status}</td>
-            <td className="col4">
-              {order.updatedAt.replace("T", "").slice(0, 10)} <br />
-              {order.updatedAt.replace("T", "").slice(10, 18)}
-            </td>
-            <td className="col5">{order.recipientName}</td>
-            <td className="col6">
-              {order.orderPostCode} {order.orderCity} <br /> {order.orderStreet} {order.orderStreetNumber}{" "}
-              {order.orderFlatNumber && `/ ${order.orderFlatNumber}`}
-            </td>
-            <td className="col7">
-              <Link href={`/updateOrder/${order.orderId}`}>
-                <Image src={EditIcon} alt={`Edytuj zamwienie nr: ${order.orderId}`} />
-              </Link>
-            </td>
-          </tr>
-        );
+        return <TableDataRow key={order.orderId} order={order}/>;
       })
     );
 
-    console.log(response);
+    
   }
 
   return (
@@ -79,66 +53,7 @@ export default function Dashboard() {
                 <div className="col7 th">Opcje</div>
               </div>
             </div>
-            <div className="tbody">
-              {/* {userOrders} */}
-              <div className="tr">
-                <div className="mainInfo">
-                  <div className="col1 td">
-                    <input type="checkbox" />
-                  </div>
-                  <div className="col2 td">0407bfbf-378d-4b6b-b1b0-2ec3ee7af76e</div>
-                  <div className="col3 td">123456789</div>
-                  <div className="col4 td">123456789</div>
-                  <div className="col5 td">123456789</div>
-                  <div className="col6 td">05-077 Warszawa, Graniczna 15A</div>
-                  <div className="col7 td">
-                    <Link href={`/updateOrder`}>
-                      <Image src={EditIcon} alt={`Edytuj zamwienie nr: `} />
-                    </Link>
-                    <label htmlFor="showMore">
-                      <input type="checkbox" id="showMore" />
-                      <Image src={CircleArrowDownIcon} alt={`Rozwiń zamwienie nr: `} />
-                    </label>
-                  </div>
-                </div>
-                <div className="detailInfo">
-                  <div className="tr">
-                    <div className="addInfoHeader">
-                      <p>Dodatkowe Informacje</p>
-                    </div>
-                    <div className="additionalInfo">
-                      <Image src={PhoneIcon} alt={`Numer telefonu: `} />
-                      <span>123456789</span>
-                    </div>
-                    <div className="additionalInfo">
-                      <Image src={EmailIcon} alt={`Adres Email: `} />
-                      <span>test@test.pl</span>
-                    </div>
-                    <div className="additionalInfo">
-                      <Image src={CompanyIcon} alt={`Dostawa od firmy: `} />
-                      <span>Agata Meble sp. z o.o.</span>
-                    </div>
-                  </div>
-                  <div className="tr">
-                    <div className="addInfoHeader">
-                      <p>Wykaz Paczek</p>
-                      <p>5</p>
-                    </div>
-                    <div className="packages">
-                      
-                    </div>
-                  </div>
-                  <div className="tr">
-                    <div className="addInfoHeader">
-                      <p>Notatka</p>
-                    </div>
-                    <div className="note">
-                      <p>Bardzo długa notatka na temat szczegółow transpoirtu przesyłi napisana przez klienta</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <div className="tbody">{userOrders}</div>
           </div>
         </main>
       </div>
---
+++ src/app/(Website)/components/TableDataRow.js
@@ -0,0 +1,108 @@
+import Image from "next/image";
+import Link from "next/link";
+import EditIcon from "@/images/icons/editIcon.png";
+import CircleArrowDownIcon from "@/images/icons/circleArrowDownIcon.png";
+import PhoneIcon from "@/images/icons/phoneIcon.png";
+import EmailIcon from "@/images/icons/emailIcon.png";
+import CompanyIcon from "@/images/icons/companyIcon.png";
+
+export default function TableDataRow({
+  order,
+  orderId,
+  orderStatus,
+  orderUpdate,
+  orderRecipient,
+  orderPostCode,
+  orderCity,
+  orderStreet,
+  orderStreetNumber,
+  orderFlatNumber,
+  orderRecipientNumber,
+  orderRecipientEmail,
+  orderFromCompany,
+
+  orderRecipientAddress,
+  orderRecipientContact,
+  orderRecipientCompany,
+  orderPackages,
+}) {
+  console.log(order);
+  return (
+    <div className="tr">
+      <div className="mainInfo">
+        <div className="col1 td">
+          <input type="checkbox" />
+        </div>
+        <div className="col2 td">{order.orderId}</div>
+        <div className="col3 td">{order.status}</div>
+        <div className="col4 td">
+          {order.updatedAt.replace("T", "").slice(0, 10)} <br />
+          {order.updatedAt.replace("T", "").slice(10, 18)}
+        </div>
+        <div className="col5 td">{order.recipientName}</div>
+        <div className="col6 td">
+          {order.orderPostCode} {order.orderCity} <br /> {order.orderStreet} {order.orderStreetNumber}{" "}
+          {order.orderFlatNumber && `/ ${order.orderFlatNumber}`}
+        </div>
+        <div className="col7 td">
+          <Link href={`/updateOrder/${order.orderId}`}>
+            <Image src={EditIcon} alt={`Edytuj zamwienie nr: ${order.orderId}`} />
+          </Link>
+          <label className="showMoreLabel">
+            <input type="checkbox" className="showMoreInput" />
+            <Image src={CircleArrowDownIcon} alt={`Rozwiń zamwienie nr: ${order.orderId}`} />
+          </label>
+        </div>
+      </div>
+      <div className="detailInfo">
+        <div className="tr">
+          <div className="addInfoHeader">
+            <p>Dodatkowe Informacje</p>
+          </div>
+          <div className="addInfoContent">
+            <div className="additionalInfo">
+              <Image src={PhoneIcon} alt={`Numer telefonu`} />
+              <p>{order.recipientPhone}</p>
+            </div>
+            <div className="additionalInfo">
+              <Image src={EmailIcon} alt={`Adres Email: `} />
+              <p>{order.recipientEmail}</p>
+            </div>
+            <div className="additionalInfo">
+              <Image src={CompanyIcon} alt={`Dostawa od firmy: `} />
+              <p>{order.user.company}</p>
+            </div>
+          </div>
+        </div>
+        <div className="tr">
+          <div className="addInfoHeader">
+            <p className="title">Ilość Paczek</p>
+            <p className="counter">{order.packages.length}</p>
+          </div>
+          <div className="addInfoContent col">
+            {order.packages.map((packageItem) => {
+              return (
+                <div className="row" key={packageItem.packageId}>
+                  <p className="rowData1 rowData">{packageItem.packageId}</p>
+                  <p className="rowData2 rowData">{packageItem.commodityName}</p>
+                  <p className="rowData3 rowData">
+                    {packageItem.commodityPaymentType === "Pobranie" ? packageItem.commodityPrice : "Opłacona"}
+                  </p>
+                  <p className="rowData4 rowData">{packageItem.commodityNote}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="tr">
+          <div className="addInfoHeader">
+            <p>Notatka</p>
+          </div>
+          <div className="addInfoContent note">
+            <p>{order.orderNote}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
